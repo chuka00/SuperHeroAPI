c0cd5668b1b0323e43352beab3492ee48b87eb82
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-17 23:05:13 +0100

    Added and modified controller

--- SuperHeroCrud/Controllers/SuperHeroController.cs
+++ SuperHeroCrud/Controllers/SuperHeroController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperHeroCrud.Data;
+using System.Data.SqlClient;
 
 namespace SuperHeroCrud.Controllers
 {
@@ -12,6 +15,23 @@ namespace SuperHeroCrud.Controllers
         public SuperHeroController(IConfiguration config)
         {
             _config = config;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConn"));
+            var heroes = await connection.QueryAsync<SuperHero>("select * from superheroes");
+            return Ok(heroes);
+        }
+
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<SuperHero>> GetSuperHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConn"));
+            var hero = await connection.QueryFirstAsync<SuperHero>("select * from superheroes where id = @Id", 
+                new { Id = heroId} );
+            return Ok(hero);
         }
     }
 }
