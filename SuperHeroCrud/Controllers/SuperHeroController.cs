870bc596888af6df93698c8bb4b0ff3059864fa1
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-17 22:15:21 +0100

    Add project files.

---
+++ SuperHeroCrud/Controllers/SuperHeroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
