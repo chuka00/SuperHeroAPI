c30b88daaf7ac27103d0d2ad82ccea3bb16ad0cb
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-19 13:24:08 +0100

    Simple bug fixes

--- SuperHeroCrud/Controllers/SuperHeroController.cs
+++ SuperHeroCrud/Controllers/SuperHeroController.cs
@@ -35,10 +35,7 @@ namespace SuperHeroCrud.Controllers
                 new { Id = heroId} );
             return Ok(hero);
         }
-        private static async Task<IEnumerable<SuperHeroes>> SelectAllHeroes(SqlConnection connection)
-        {
-            return await connection.QueryAsync<SuperHeroes>("select * from superheroes");
-        }
+        
 
         [HttpPost]
         public async Task<ActionResult<List<SuperHeroes>>> CreateSuperHero(SuperHeroes hero)
@@ -46,6 +43,29 @@ namespace SuperHeroCrud.Controllers
             using var connection = new SqlConnection(_config.GetConnectionString("DefaultConn"));
             await connection.ExecuteAsync("insert into superheroes(name, firstname, lastname, place) values (@Name, @FirstName, @LastName, @Place)", hero);
             return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHeroes>>> UpdateSuperHero(SuperHeroes hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConn"));
+            await connection.ExecuteAsync("update superheroes set name = @Name, firstname = @FirstName, lastname = @LastName, place = @Place where id = @Id", hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+        [HttpDelete("{heroId}")]
+        public async Task<ActionResult<List<SuperHeroes>>> DeleteSuperHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConn"));
+            await connection.ExecuteAsync("delete from superheroes where id = @Id", new {Id = heroId});
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+
+
+        private static async Task<IEnumerable<SuperHeroes>> SelectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHeroes>("select * from superheroes");
         }
 
     }
